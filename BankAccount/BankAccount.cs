9ab89e6ea84eecb076b7ae2ea5ceb48dd5120ad0
Viktor Reuterhäll <viktor.reuterhall@skola.sala.se>
2024-04-15 10:46:51 +0200

    Add project files.

---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public class BankAccount
+    {
+        
+        int value = 0;
+        public void Deposit(int amount)
+        {
+            value = value + amount; 
+           
+        }
+      
+        public void Withdraw(int amount)
+        {
+            
+            if (amount < value)
+            {
+                value = value - amount;
+            }
+            else
+            {
+                amount = 0; 
+            }
+        }
+
+        public int GetBalance()
+        {
+            
+            return value;
+        }
+    }
+
+
+}
---
+++ BankAccount/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.Metrics;
+using System.Reflection;
+
+namespace BankAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount1 = new BankAccount();
+            BankAccount bankAccount2 = new BankAccount();
+            bankAccount1.Deposit(100);
+            bankAccount2.Deposit(50);
+            Console.WriteLine("Account1: " + bankAccount1.GetBalance());
+            Console.WriteLine("Account2: " + bankAccount2.GetBalance());
+            bankAccount1.Withdraw(20);
+            bankAccount2.Deposit(70);
+            Console.WriteLine("Account1: " + bankAccount1.GetBalance());
+            Console.WriteLine("Account2: " + bankAccount2.GetBalance());
+            bankAccount1.Withdraw(9999);
+            Console.WriteLine("Account1: " + bankAccount1.GetBalance());
+        }
+
+
+    }
+}
